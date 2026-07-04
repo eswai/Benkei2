9ded9bd76281896881baf15d41168e01ac6ce9fe
eswai <@eswai>
2026-07-04 11:47:10 +0900

    有効無効の表示を変更

--- Benkei2/Benkei2App.swift
+++ Benkei2/Benkei2App.swift
@@ -37,8 +37,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     private func configureStatusMenu() {
         let menu = NSMenu()
-        let toggleItem = NSMenuItem(title: currentRemapperTitle(), action: #selector(toggleEnabled), keyEquivalent: "")
+        let toggleItem = NSMenuItem(title: "薙刀式かな入力", action: #selector(toggleEnabled), keyEquivalent: "")
         toggleItem.target = self
+        toggleItem.state = KeyRemapper.shared.isEnabled ? .on : .off
         toggleMenuItem = toggleItem
         menu.addItem(toggleItem)
         menu.addItem(NSMenuItem.separator())
@@ -48,12 +49,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.menu = menu
     }
 
-    private func currentRemapperTitle() -> String {
-        return KeyRemapper.shared.isEnabled ? "変換有効" : "変換オフ"
-    }
-
     private func refreshRemapperUI() {
-        toggleMenuItem?.title = currentRemapperTitle()
+        toggleMenuItem?.state = KeyRemapper.shared.isEnabled ? .on : .off
         updateStatusBarIcon()
     }
 
